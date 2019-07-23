714190ea41fefe0ae0c30d4ba5862c3a46dee65a
zengwenchao <zengwenchao000@163.com>
2019-07-23 19:58:52 +0800

    测试文件

---
+++ hello-git/src/com/itheima/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("git你好1...");
+        System.out.println("git你好2...");
+        System.out.println("git你好3...");
+        System.out.println("git你好4...");
+        System.out.println("git你好5...");
+        System.out.println("git你好6...");
+        System.out.println("git你好7...");
+        System.out.println("git你好8...");
+    }
+}
