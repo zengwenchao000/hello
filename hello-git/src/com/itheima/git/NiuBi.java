56e4d79968fe98fa7945d570c51f794bd0690a56
zengwenchao <zengwenchao000@163.com>
2019-07-23 20:32:51 +0800

    添加niubi

---
+++ hello-git/src/com/itheima/git/NiuBi.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class NiuBi {
+    public static void main(String[] args) {
+        System.out.println("55开牛逼");
+        System.out.println("55开牛逼");
+        System.out.println("55开牛逼");
+        System.out.println("55开牛逼");
+        System.out.println("55开牛逼");
+        System.out.println("55开牛逼");
+    }
+}
